42a121677de5bca8d212408598014fc8ba14c700
Wai Linn Aung <waia00198@gmail.com>
2024-03-14 12:28:58 +0630

    add search

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,8 +1,11 @@
 import { YStack, H2, Separator, Theme } from 'tamagui';
 
 import EditScreenInfo from '../../components/edit-screen-info';
+import { useFocusEffect } from 'expo-router';
+import { useCallback } from 'react';
 
 export default function TabTwoScreen() {
+  useFocusEffect(useCallback(() => console.log('hello'), []));
   return (
     <Theme name="light">
       <YStack flex={1} alignItems="center" justifyContent="center">
--- components/Content.tsx
+++ components/Content.tsx
@@ -1,71 +1,97 @@
 import React, { useEffect, useState } from 'react';
-import { H1, View, XStack, YStack } from 'tamagui';
+import { Button, Input, Text, View, XStack, YStack } from 'tamagui';
 import data from '../data/Recipes.json';
 import userType from '../data/UsersTypes.json';
 import Cards from './Cards';
 import { FlatList } from 'react-native';
-import { Subtitle, Title } from '~/tamagui.config';
+import { Subtitle } from '~/tamagui.config';
 import { RecipeType } from '~/interfaces/data';
+import { Ionicons } from '@expo/vector-icons';
 
 const Content = () => {
   const [selectUserType, setSelectUserType] = useState('');
-  const [active, setActive] = useState('');
+  const [search, setSearch] = useState('');
   const [filteredData, setfilteredData] = useState<RecipeType[]>([]);
 
   const selectUser = (userType: string) => {
     setSelectUserType(userType);
-    setActive(userType);
     console.log(userType);
   };
 
   const filterData = () => {
-    const recipe = data.filter((el) => el.UserType === selectUserType);
-    setfilteredData(recipe);
+    if (search.length > 0) {
+      const searchRecipe = data.filter((el) => el.Name.includes(search));
+      setfilteredData(searchRecipe);
+    } else if (selectUserType !== '') {
+      const recipe = data.filter((el) => el.UserType === selectUserType);
+      setfilteredData(recipe);
+    } else {
+      setfilteredData(data);
+    }
   };
 
   useEffect(() => {
     filterData();
-  }, [selectUserType]);
+  }, [selectUserType, search]);
 
   return (
-    <YStack>
-      <XStack gap={'$3'} marginVertical={'$2'} paddingLeft={'$3'}>
-        <View
-          backgroundColor={`${active === '' ? '$yellow5Dark' : '$gray4Light'}`}
-          padding={'$2'}
-          borderRadius={8}>
-          <Subtitle
-            color={`${active === '' ? 'white' : '$gray3Dark'}`}
-            size={'$4'}
-            onPress={() => selectUser('')}>
-            🤤 အကုန်စား
-          </Subtitle>
-        </View>
-        {userType.map((user) => (
+    <YStack marginBottom={'$13'}>
+      <View overflow="hidden" height={search === '' ? 100 : 50}>
+        <XStack ai={'center'} position="relative">
+          <Input
+            size="$3"
+            borderWidth={1}
+            w={'95%'}
+            ml={'$3'}
+            mr={'$1'}
+            marginVertical={'$2'}
+            bc={'$gray3Light'}
+            boc={'$yellow7Light'}
+            color={'#000'}
+            fontWeight={'bold'}
+            onChangeText={(text: string) => setSearch(text)}
+          />
+          <Ionicons
+            name="search"
+            color={'hsl(47, 100%, 50.0%)'}
+            size={24}
+            style={{ position: 'absolute', right: 20 }}
+          />
+        </XStack>
+        <XStack space={'$3'} marginVertical={'$2'} paddingLeft={'$3'}>
           <View
-            key={user.UserId}
-            backgroundColor={`${user.UserCode === active ? '$yellow5Dark' : '$gray4Light'}`}
+            backgroundColor={`${selectUserType === '' ? '$yellow5Dark' : '$gray4Light'}`}
             padding={'$2'}
-            borderRadius={5}>
+            borderRadius={8}>
             <Subtitle
-              color={`${user.UserCode === active ? 'white' : '$gray3Dark'}`}
+              color={`${selectUserType === '' ? 'white' : '$gray3Dark'}`}
               size={'$4'}
-              onPress={() => selectUser(user.UserCode)}>
-              {user.UserMMType === 'အသားစား' ? '🥩' : '🥦'} {user.UserMMType}
+              onPress={() => selectUser('')}>
+              🤤 အကုန်စား
             </Subtitle>
           </View>
-        ))}
-      </XStack>
+          {userType.map((user) => (
+            <View
+              key={user.UserId}
+              backgroundColor={`${user.UserCode === selectUserType ? '$yellow5Dark' : '$gray4Light'}`}
+              padding={'$2'}
+              borderRadius={5}>
+              <Subtitle
+                color={`${user.UserCode === selectUserType ? 'white' : '$gray3Dark'}`}
+                size={'$4'}
+                onPress={() => selectUser(user.UserCode)}>
+                {user.UserMMType === 'အသားစား' ? '🥩' : '🥦'} {user.UserMMType}
+              </Subtitle>
+            </View>
+          ))}
+        </XStack>
+      </View>
       <FlatList
-        data={selectUserType === '' ? data : filteredData}
+        data={filteredData}
         horizontal={false}
         numColumns={2}
         renderItem={({ item }) => <Cards item={item} />}
         keyExtractor={(item) => item.Guid}
-        contentContainerStyle={{
-          paddingLeft: 2,
-          paddingBottom: 80,
-        }}
       />
     </YStack>
   );
